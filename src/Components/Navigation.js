c42c3a5af358a6c3b88ca27db7aeae2341bfbaf5
Nikola Stojiljkovic <nikolavla@gmail.com>
2019-01-20 17:26:40 +0000

    Fix a within a tag

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Icon, Menu, Sidebar } from 'semantic-ui-react'
 
+const LinkItem = (props) => (
+  <Link className={props.className} to='/dashboard'>
+    <Icon name='dashboard' />
+    Dashboard
+  </Link>
+);
+
 export default class Navigation extends Component {
   state = { visible: true }
 
@@ -19,11 +26,7 @@ export default class Navigation extends Component {
             visible={visible}
             width='thin'
           >
-            <Menu.Item as='a'>
-              <Link to='/dashboard'>
-                <Icon name='dashboard' />
-                Dashboard
-              </Link>
+            <Menu.Item as={LinkItem}>
             </Menu.Item>
           </Sidebar>
     );
--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const NotFoundPage = () => (
 	<p>404 - Page not found</p>
